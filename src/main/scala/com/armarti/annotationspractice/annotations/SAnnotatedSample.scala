a672f52625d36f452ff9dda687acecc25e0d5959
David Charles Morse <dcmorse@armarti.com>
2019-12-26 22:59:05 +0000

    trashed scala example

--- src/main/scala/com/armarti/annotationspractice/annotations/SAnnotatedSample.scala
+++
@@ -1,9 +0,0 @@
-package com.armarti.annotationspractice.annotations
-
-@SAnnotationSample(
-  required = new SAnnotationUsedInOtherAnnotation(id = 3, name = "nom"),
-  req = "req'd field"
-)
-class SAnnotatedSample {
-  def doStuff(): Unit = println("I'm doing stuff.")
-}
--- src/main/scala/com/armarti/annotationspractice/annotations/SAnnotationUsedInOtherAnnotation.scala
+++
@@ -1,9 +0,0 @@
-package com.armarti.annotationspractice.annotations
-
-import scala.annotation.StaticAnnotation
-
-class SAnnotationUsedInOtherAnnotation(
-  id: Int,
-  name: String,
-  description: String = "default description"
-) extends StaticAnnotation
